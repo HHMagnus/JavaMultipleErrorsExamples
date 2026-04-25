91079ef534129d12e8cc6c08cfab2322528ec7a7
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2026-04-25 11:13:08 +0200

    function5 for throw

--- src/test/java/dev/mhh/multiple_errors_example/CombineThrowTest.java
+++ src/test/java/dev/mhh/multiple_errors_example/CombineThrowTest.java
@@ -40,4 +40,18 @@ class CombineThrowTest {
 
         assertEquals(100, combined);
     }
+
+    @Test
+    void combineThrow5() throws CombineThrow.AggregatedException {
+        final var combined = CombineThrow.exceptionAggregator(
+                (t1, t2, t3, t4, t5) -> t1 + t2 + t3 + t4 + t5,
+                () -> 10L,
+                () -> 20L,
+                () -> 30L,
+                () -> 40L,
+                () -> 50L
+        );
+
+        assertEquals(150, combined);
+    }
 }
