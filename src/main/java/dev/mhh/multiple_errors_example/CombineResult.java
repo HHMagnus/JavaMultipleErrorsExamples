31f216381147d7f74479c8840b8b43209dbbda47
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2026-04-25 11:00:47 +0200

    function3 for result

--- src/main/java/dev/mhh/multiple_errors_example/CombineResult.java
+++ src/main/java/dev/mhh/multiple_errors_example/CombineResult.java
@@ -1,10 +1,11 @@
 package dev.mhh.multiple_errors_example;
 
+import dev.mhh.multiple_errors_example.functions.Function2;
+import dev.mhh.multiple_errors_example.functions.Function3;
 import dev.mhh.result.Result;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BiFunction;
 
 public class CombineResult {
     public interface VarFunction<T> {
@@ -32,11 +33,22 @@ public class CombineResult {
 
     @SuppressWarnings("unchecked")
     public static <T1, T2, T, E> Result<T, List<E>> combine(
-            BiFunction<T1, T2, T> function,
+            Function2<T1, T2, T> function,
             Result<T1, E> result1,
             Result<T2, E> result2
     ) {
         final VarFunction<T> varFunction = (args) -> function.apply((T1)args[0], (T2)args[1]);
         return combine(varFunction, (Result<Object, E>) result1, (Result<Object, E>) result2);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T1, T2, T3, T, E> Result<T, List<E>> combine(
+            Function3<T1, T2, T3, T> function,
+            Result<T1, E> result1,
+            Result<T2, E> result2,
+            Result<T3, E> result3
+    ) {
+        final VarFunction<T> varFunction = (args) -> function.apply((T1)args[0], (T2)args[1], (T3)args[2]);
+        return combine(varFunction, (Result<Object, E>) result1, (Result<Object, E>) result2, (Result<Object, E>) result3);
     }
 }
--- src/test/java/dev/mhh/multiple_errors_example/CombineResultTest.java
+++ src/test/java/dev/mhh/multiple_errors_example/CombineResultTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CombineResultTest {
     @Test
-    void testCombineResult() {
+    void testCombineResult2() {
         final var result = CombineResult.combine(
                 Long::sum,
                 Result.ok(123L),
@@ -17,5 +17,17 @@ class CombineResultTest {
 
         assertTrue(result.isOk());
         assertEquals(444L, result.optionalValue().get());
+    }
+    @Test
+    void testCombineResult3() {
+        final var result = CombineResult.combine(
+                (x, y, z) -> x + y + z,
+                Result.ok(123L),
+                Result.ok(321L),
+                Result.ok(111L)
+        );
+
+        assertTrue(result.isOk());
+        assertEquals(555L, result.optionalValue().get());
     }
 }
